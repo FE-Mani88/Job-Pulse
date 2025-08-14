98e15acfc6ceba39782c329b5f52a5ebd12842a0
Mani <khm750298@gmail.com>
2025-08-14 13:55:28 +0430

    before Handling Ticketing

--- src/app/admin-panel/page.js
+++ src/app/admin-panel/page.js
@@ -472,7 +472,7 @@ export default function Page() {
             </TabsList>
 
             {/* Data Cards  */}
-            <div className="mt-1.5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-4 xl:px-0">
+            <div className="mt-1.5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
               <Card className="gap-4">
                 <CardHeader>
                   <CardTitle className="text-lg">Total Revenue</CardTitle>
